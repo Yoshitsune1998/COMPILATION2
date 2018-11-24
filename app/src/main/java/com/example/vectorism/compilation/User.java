43dfba26c75c811609ed629837b8699ea5e7210f
Abdul Yasir Halomoan <smpyasir01@gmail.com>
2018-11-25 01:21:36 +0700

    user class

---
+++ app/src/main/java/com/example/vectorism/compilation/User.java
@@ -0,0 +1,16 @@
+package com.example.vectorism.compilation;
+
+public class User {
+
+    private int idUser;
+    private int idImage;
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String email,String password){
+        this.email = email;
+        this.password = password;
+    }
+
+}
