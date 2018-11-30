45bb52ac6fa4ab2c2097f556ce99666e373c3871
Abdul Yasir Halomoan <smpyasir01@gmail.com>
2018-11-30 21:41:27 +0700

    minor change on registration

--- app/src/main/java/com/example/vectorism/compilation/Register.java
+++ app/src/main/java/com/example/vectorism/compilation/Register.java
@@ -20,6 +20,8 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
 
     EditText email;
     EditText password;
+    EditText username;
+    EditText password2;
     Button reg_button;
     ProgressDialog p_dialog;
 
@@ -32,8 +34,10 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
 
         auth = FirebaseAuth.getInstance();
 
-        email = (EditText) findViewById(R.id.i_email);
-        password = (EditText) findViewById(R.id.i_password);
+        email = (EditText) findViewById(R.id.l_email);
+        password = (EditText) findViewById(R.id.l_password);
+        username = (EditText) findViewById(R.id.l_username);
+        password2 = (EditText) findViewById(R.id.l_conpassword);
         reg_button = (Button) findViewById(R.id.reg_button);
         reg_button.setOnClickListener(this);
         p_dialog = new ProgressDialog(this);
@@ -47,9 +51,11 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
     }
 
     private void Register(){
-        String u = email.getText().toString().trim();
-        String p = password.getText().toString().trim();
-        if (TextUtils.isEmpty(u)) {
+        final String e = email.getText().toString().trim();
+        final String p = password.getText().toString().trim();
+        final String u = username.getText().toString().trim();
+        final String p2 = password2.getText().toString().trim();
+        if (TextUtils.isEmpty(e)) {
             Log.d("USERNAME", "Empty");
             Toast.makeText(this, "Please Input Email", Toast.LENGTH_SHORT).show();
             return;
@@ -59,14 +65,25 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
             Toast.makeText(this, "Please Input Password", Toast.LENGTH_SHORT).show();
             return;
         }
+        if (TextUtils.isEmpty(p2)) {
+            Log.d("PASSWORD", "Empty");
+            Toast.makeText(this, "Please Input the Confirmation Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(p!=p2){
+            Log.d("PASSWORD", "Different");
+            Toast.makeText(this, "Please Input the Same Password on Confirmation Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
         p_dialog.setMessage("Registering User...");
         p_dialog.show();
-        auth.createUserWithEmailAndPassword(u, p).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        auth.createUserWithEmailAndPassword(e, p).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 p_dialog.dismiss();
                 if (task.isComplete()) {
                     Toast.makeText(Register.this, "Registered Complete", Toast.LENGTH_SHORT).show();
+                    User user = new User(u,e,p);
                     backToLogin();
                 } else {
                     Toast.makeText(Register.this, "Register Failed", Toast.LENGTH_SHORT).show();
